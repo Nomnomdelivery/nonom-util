4982c1c1c4c24535672d469005cc736b932ef8b1
cloderaldo <cloderaldo@gmail.com>
2026-02-21 11:23:26 +0800

    text image

--- lib/widgets/store_promo_viewer.dart
+++ lib/widgets/store_promo_viewer.dart
@@ -37,11 +37,10 @@ class _StorePromoViewerState extends ConsumerState<StorePromoViewer> {
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  //TODO: fix image
-                  // Image.asset(
-                  //   "packages/nomnom_util/assets/images/rider.png",
-                  //   height: 120,
-                  // ),
+                  Image.asset(
+                    "packages/nomnom_util/assets/images/rider.png",
+                    height: 120,
+                  ),
                   const Gap(20),
                   Text("No promos generated yet."),
                 ],
