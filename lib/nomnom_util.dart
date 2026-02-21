a4582d32db2cf1252f2474d102e3fdc9893c4b42
cloderaldo <cloderaldo@gmail.com>
2026-02-21 15:28:41 +0800

    add export

--- lib/nomnom_util.dart
+++ lib/nomnom_util.dart
@@ -1,5 +1,6 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'api/base_store_api.dart';
+export 'api/cart_api.dart';
+export 'api/app_abstract.dart';
+export 'api/auth_api.dart';
+export 'api/data_cacher.dart';
+export 'api/firebase_firestore_support.dart';
