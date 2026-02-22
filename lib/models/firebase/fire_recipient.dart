ede1767cf0f1d886461b73e23aa5386c75655691
cloderaldo <cloderaldo@gmail.com>
2026-02-22 08:22:14 +0800

    chore: updated firerecipient

--- lib/models/firebase/fire_recipient.dart
+++ lib/models/firebase/fire_recipient.dart
@@ -1,14 +1,20 @@
 class FireRecipient {
   final String contactNumber;
   final String name;
+  final String orderedBy;
 
-  FireRecipient({required this.contactNumber, required this.name});
+  FireRecipient({
+    required this.contactNumber,
+    required this.name,
+    required this.orderedBy,
+  });
 
   // Factory method to create an instance from Firestore document data
   factory FireRecipient.fromFirestore(Map<String, dynamic> data) {
     return FireRecipient(
       contactNumber: data['contact_number'] as String,
       name: data['name'] as String,
+      orderedBy: data['ordered_by'] as String,
     );
   }
 
