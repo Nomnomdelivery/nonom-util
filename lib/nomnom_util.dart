98db8af821ba844ec7f2f585cca60d3913d773fe
cloderaldo <cloderaldo@gmail.com>
2026-03-09 19:04:36 +0800

    export replaced badge

--- lib/nomnom_util.dart
+++ lib/nomnom_util.dart
@@ -4,3 +4,4 @@ export 'api/base_app_api.dart';
 export 'api/base_auth_api.dart';
 export 'api/base_data_cacher.dart';
 export 'api/base_firebase_firestore_support.dart';
+export 'widgets/replaced_badge.dart';
