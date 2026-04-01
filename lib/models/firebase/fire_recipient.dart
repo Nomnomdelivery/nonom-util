e5aba835e5760e790e97eae1838a736a255d15dc
cloderaldo <cloderaldo@gmail.com>
2026-04-01 12:41:50 +0800

    fire recep

--- lib/models/firebase/fire_recipient.dart
+++ lib/models/firebase/fire_recipient.dart
@@ -3,12 +3,12 @@ import 'package:nomnom_util/models/firebase/ordered_by.dart';
 class FireRecipient {
   final String contactNumber;
   final String name;
-  final OrderedBy orderedBy;
+  final OrderedBy? orderedBy;
 
   FireRecipient({
     required this.contactNumber,
     required this.name,
-    required this.orderedBy,
+    this.orderedBy,
   });
 
   // Factory method to create an instance from Firestore document data
@@ -16,12 +16,22 @@ class FireRecipient {
     return FireRecipient(
       contactNumber: data['contact_number'] as String,
       name: data['name'] as String,
-      orderedBy: OrderedBy.fromFirestore(data['ordered_by']),
+      orderedBy: data['ordered_by'] != null
+          ? OrderedBy.fromFirestore(data['ordered_by'] as Map<String, dynamic>)
+          : null,
     );
   }
 
   // Method to convert the instance to Firestore document data
   Map<String, dynamic> toFirestore() {
-    return {'contact_number': contactNumber, 'name': name};
+    return {
+      'contact_number': contactNumber,
+      'name': name,
+      if (orderedBy != null)
+        'ordered_by': {
+          'contact_number': orderedBy!.contactNumber,
+          'full_name': orderedBy!.fullName,
+        },
+    };
   }
 }
